1910f4587b32135da389b240c91253d8d4f76039
Volter Dalzon <vdalzon@gmail.com>
2020-03-30 22:27:36 -0400

    added card style to drinks

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 let movieDetail = document.getElementById("card-movie-desc");
-
+let drinkDetail = document.getElementById("card-drink-desc");
 let limitChar = function() {
     movieDetail.innerHTML =
         movieDetail.innerHTML.substring(0, 1200) +
         "...  <a class='read-more' href='#'>READ MORE</a>  ";
+
+    drinkDetail.innerHTML =
+        drinkDetail.innerHTML.substring(0, 1200) +
+        "...  <a class='read-more drink-detail-shift' href='#'>READ MORE</a>  ";
 };
 limitChar();
